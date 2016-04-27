a5cd68d33f88ab181aef29df1b490bb2709ef672
Nicolas Wolf <nicolas.wolf@fau.de>
2016-04-27 21:23:33 +0200

    1st commit

---
+++ src/main/java/de/fau/cs/mad/wanthavers/server/ServerApplication.java
@@ -0,0 +1,11 @@
+package de.fau.cs.mad.wanthavers.server;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class ServerApplication extends Application<ServerConfiguration> {
+    @Override
+    public void run(ServerConfiguration configuration, Environment environment) throws Exception {
+
+    }
+}
---
+++ src/main/java/de/fau/cs/mad/wanthavers/server/ServerConfiguration.java
@@ -0,0 +1,7 @@
+package de.fau.cs.mad.wanthavers.server;
+
+import io.dropwizard.Configuration;
+
+public class ServerConfiguration extends Configuration {
+
+}
