053b6b255593a5ff613a34f0877040ce0df68680
Nicolas Wolf <nicolas.wolf@fau.de>
2016-06-11 16:04:05 +0200

    use auth for cloud message tokens

--- src/main/java/de/fau/cs/mad/wanthavers/server/impl/CloudMessageTokenResourceImpl.java
+++ src/main/java/de/fau/cs/mad/wanthavers/server/impl/CloudMessageTokenResourceImpl.java
@@ -2,8 +2,10 @@ package de.fau.cs.mad.wanthavers.server.impl;
 
 
 import de.fau.cs.mad.wanthavers.common.CloudMessageToken;
+import de.fau.cs.mad.wanthavers.common.User;
 import de.fau.cs.mad.wanthavers.common.rest.api.CloudMessageTokenResource;
 import de.fau.cs.mad.wanthavers.server.facade.CloudMessageTokenFacade;
+import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
 import io.swagger.annotations.ApiParam;
 
@@ -20,20 +22,20 @@ public class CloudMessageTokenResourceImpl implements CloudMessageTokenResource 
 
     @Override
     @UnitOfWork
-    public List<CloudMessageToken> getAllTokens(@ApiParam(value = "id of the desired user", required = true) long userId) {
-        return facade.getAllTokens(userId);
+    public List<CloudMessageToken> getAllTokens(@Auth User user) {
+        return facade.getAllTokens(user.getID());
     }
 
     @Override
     @UnitOfWork
-    public CloudMessageToken createToken(@ApiParam(value = "id of the desired user", required = true) long userId, @ApiParam(value = "Token to create", required = true) CloudMessageToken newToken) {
-        return facade.createNewToken(userId, newToken);
+    public CloudMessageToken createToken(@Auth User user, @ApiParam(value = "Token to create", required = true) CloudMessageToken newToken) {
+        return facade.createNewToken(user.getID(), newToken);
     }
 
     @Override
     @UnitOfWork
-    public CloudMessageToken get(@ApiParam(value = "id of the desired user", required = true) long userId, @ApiParam(value = "id of the token", required = true) long id) {
-        CloudMessageToken token = facade.getTokenById(userId, id);
+    public CloudMessageToken get(@Auth User user, @ApiParam(value = "id of the token", required = true) long id) {
+        CloudMessageToken token = facade.getTokenById(user.getID(), id);
 
         if(token == null)
             throw new WebApplicationException(404);
@@ -43,13 +45,13 @@ public class CloudMessageTokenResourceImpl implements CloudMessageTokenResource 
 
     @Override
     @UnitOfWork
-    public CloudMessageToken updateToken(@ApiParam(value = "id of the desired user", required = true) long userId, @ApiParam(value = "id of the token", required = true) long id, @ApiParam(value = "new details of the specified token", required = true) CloudMessageToken token) {
-        return facade.updateToken(userId, id, token);
+    public CloudMessageToken updateToken(@Auth User user, @ApiParam(value = "id of the token", required = true) long id, @ApiParam(value = "new details of the specified token", required = true) CloudMessageToken token) {
+        return facade.updateToken(user.getID(), id, token);
     }
 
     @Override
     @UnitOfWork
-    public void deleteToken(@ApiParam(value = "id of the desired user", required = true) long userId, @ApiParam(value = "id of the to be deleted token", required = true) long id) {
-        facade.deleteToken(userId, id);
+    public void deleteToken(@Auth User user, @ApiParam(value = "id of the to be deleted token", required = true) long id) {
+        facade.deleteToken(user.getID(), id);
     }
 }
