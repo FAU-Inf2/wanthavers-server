7d79b185f602f18960555928f92410abff2c0ba2
H0schi <thorsten-isaac@gmx.net>
2016-06-11 12:54:57 +0200

    try and get chat over parseobject objectid

--- src/main/java/de/fau/cs/mad/wanthavers/server/dao/ChatDAO.java
+++ src/main/java/de/fau/cs/mad/wanthavers/server/dao/ChatDAO.java
@@ -3,8 +3,6 @@ package de.fau.cs.mad.wanthavers.server.dao;
 import de.fau.cs.mad.wanthavers.common.Chat;
 import de.fau.cs.mad.wanthavers.common.Message;
 import de.fau.cs.mad.wanthavers.common.User;
-import de.fau.cs.mad.wanthavers.server.SingletonManager;
-import de.fau.cs.mad.wanthavers.server.facade.UserFacade;
 import de.fau.cs.mad.wanthavers.server.parse.mapper.ChatMapper;
 import de.fau.cs.mad.wanthavers.server.parse.mapper.MessageMapper;
 import de.fau.cs.mad.wanthavers.server.parse.models.ParseChat;
@@ -52,11 +50,10 @@ public class ChatDAO{
         c.setDesireId(desireId);
         c.setUser1(u1);
         c.setUser2(u2);
-        c.setChatId(c.getObjectId());
 
         try {
             c.save();
-            Chat chat = getChatByChatId(c.getChatId());
+            Chat chat = getChatByChatId(c.getObjectId());
             System.out.println("Chat: User1 "+chat.getUser1()+", User2 "+chat.getUser2());
             return ChatMapper.get(c);
         } catch (ParseException e) {
@@ -121,7 +118,7 @@ public class ChatDAO{
 
     public Chat getChatByChatId(String chatId) {
         ParseQuery<ParseChat> query = ParseQuery.getQuery(ParseChat.class);
-        query.whereEqualTo(ParseChat.chatId, chatId);
+        query.whereEqualTo("objectId", chatId);
 
         List<Chat> list = new ArrayList<>();
         try {
--- src/main/java/de/fau/cs/mad/wanthavers/server/parse/models/ParseChat.java
+++ src/main/java/de/fau/cs/mad/wanthavers/server/parse/models/ParseChat.java
@@ -9,7 +9,6 @@ public class ParseChat extends ParseObject {
     public static String user1 = "user1";
     public static String user2 = "user2";
     public static String desireId = "desireId";
-    public static String chatId = "chatId";
 
     public void setUser1(long value) {
         put(user1, value);
@@ -33,14 +32,6 @@ public class ParseChat extends ParseObject {
 
     public long getDesireId() {
         return getLong(desireId);
-    }
-
-    public void setChatId(String value) {
-        put(chatId, value);
-    }
-
-    public String getChatId() {
-        return getString(chatId);
     }
 
 }
