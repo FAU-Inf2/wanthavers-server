8a2c78b340ec74fc95682478b4ff4ea2ad27228f
philipp <philsippl@gmail.com>
2016-06-27 15:55:39 +0200

    null check in category name sorter

--- src/main/java/de/fau/cs/mad/wanthavers/server/sort/CategoryNameComperator.java
+++ src/main/java/de/fau/cs/mad/wanthavers/server/sort/CategoryNameComperator.java
@@ -8,6 +8,11 @@ import java.util.Comparator;
 public class CategoryNameComperator implements Comparator<Category> {
     @Override
     public int compare(Category o1, Category o2) {
+        if(o1 == null)
+            return -1;
+
+        if(o2 == null)
+            return 1;
         return o1.getName().compareTo(o2.getName());
     }
 }
