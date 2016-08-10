74674af2d1df13325e703ba91af6922f75695d4d
H0schi <thorsten-isaac@gmx.net>
2016-08-10 22:38:12 +0200

    added whitespace to push notification message on haver reject

--- src/main/java/de/fau/cs/mad/wanthavers/server/tasks/CreateStringsTask.java
+++ src/main/java/de/fau/cs/mad/wanthavers/server/tasks/CreateStringsTask.java
@@ -24,7 +24,7 @@ public class CreateStringsTask extends SessionContextTask {
         setTranslation("HAVER_ACCEPTED_NOTIFICATION_BODY", "You were accepted for {{desireTitle}} by {{desireCreator}}.", EN);
         setTranslation("HAVER_ACCEPTED_NOTIFICATION_BODY", "Du wurdest für den Job {{desireTitle}} von {{desireCreator}} angenommen.", DE);
 
-        setTranslation("HAVER_REJECTED_NOTIFICATION_BODY", "Sorry! You didn't get the job {{desireTitle}}by {{desireCreator}}. Better luck next time!", EN);
+        setTranslation("HAVER_REJECTED_NOTIFICATION_BODY", "Sorry! You didn't get the job {{desireTitle}} by {{desireCreator}}. Better luck next time!", EN);
         setTranslation("HAVER_REJECTED_NOTIFICATION_BODY", "Sorry! Du hast den Job {{desireTitle}} von {{desireCreator}} nicht bekommen. Viel Glück beim nächsten mal!", DE);
 
         setTranslation("DESIRE_COMPLETE_NOTIFICATION_BODY", "Desire {{desire}} was completed. Rate now.", EN);
