c732e4c2039e9105cbdb2359195c1d77f8805f53
H0schi <thorsten-isaac@gmx.net>
2016-08-28 20:30:31 +0200

    added pushy for sending push to apns; careful: keystore file for apns auth is missing

--- src/main/java/de/fau/cs/mad/wanthavers/server/cloudmessaging/CloudMessage.java
+++ src/main/java/de/fau/cs/mad/wanthavers/server/cloudmessaging/CloudMessage.java
@@ -8,17 +8,23 @@ public class CloudMessage {
     private long userId;
     private JSONObject data;
 
+
+    public CloudMessage(long userId, String subject, String message) {
+        this(userId, subject, message, null);
+    }
     /**
      * @param userId: id of receiver
      * @param subject: data field used for storing a String to identify further procedure of the app for this notification
      * @param message: message to display in notification center
+     * @param title: title to display in notification center
      */
-    public CloudMessage(long userId, String subject, String message) {
+    public CloudMessage(long userId, String subject, String message, String title) {
         this.userId = userId;
 
         data = new JSONObject();
         data.put("subject", subject);
         data.put("message", message);
+        data.put("title", title);
     }
 
     public long getUserId() {
@@ -27,6 +33,18 @@ public class CloudMessage {
 
     public void setUserId(long userId) {
         this.userId = userId;
+    }
+
+    public String getSubject() {
+        return data.getString("subject");
+    }
+
+    public String getMessage() {
+        return data.getString("message");
+    }
+
+    public String getTitle() {
+        return data.getString("title");
     }
 
     public void addKeyValue(String key, Object value) {
@@ -39,6 +57,14 @@ public class CloudMessage {
 
     public void setData(JSONObject data) {
         this.data = data;
+    }
+
+    public JSONObject getDataWithoutHeader() {
+        JSONObject dataCopy = new JSONObject(data);
+        dataCopy.remove("subject");
+        dataCopy.remove("message");
+        dataCopy.remove("title");
+        return dataCopy;
     }
 
 }
